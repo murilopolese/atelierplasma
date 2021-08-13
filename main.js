186f907bf7d01811c800897670dea512c3bb323f
Murilo Polese <murilopolese@gmail.com>
2021-08-13 07:09:11 +0200

    Adding letter by letter javascript animation

---
+++ main.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+  // Wrap letters in spans
+  var menuItems = document.querySelectorAll('a')
+  for (var i = 0; i < menuItems.length; i++) {
+    var item = menuItems[i]
+    var content = item.innerText
+    var newContent = ''
+    for (var j = 0; j < content.length; j++) {
+      newContent += "<span>"+content[j]+"</span>"
+    }
+    item.innerHTML = newContent
+  }
+  // Reveal letters
+  function revealLetter(n) {
+    return function() {
+      var letter = letters[n]
+      letter.style.opacity = 1
+    }
+  }
+  var letters = document.querySelectorAll('a span')
+  for (var i = 0; i < letters.length; i++) {
+    setTimeout(revealLetter(i), i*100)
+  }
+}
